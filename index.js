9463f1e62a187fe5be7128dc500a67db914cfc6b
Eddy-Lugonze <eddylugonze@gmail.com>
2025-03-13 18:10:31 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+// index.js
+function superbowlWin(record) {
+    // Use the Array.prototype.find method to locate the first win
+    const winningRecord = record.find(game => game.result === "W");
+    // Return the year if a winning record is found, otherwise return undefined
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
+  // Export the function for use in other files
+  module.exports = superbowlWin;
